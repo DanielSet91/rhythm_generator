e996d0c67e53ee7dac547a67706de1af4b1bc185
DanielSet91 <Elessar.nazgul@gmail.com>
2024-01-05 20:03:43 +0200

    triplets bug fix, raising errors

--- test.py
+++ test.py
@@ -1,26 +1,19 @@
 from music21 import *
 
-note1 = note.Note('C4')
-note1.quarterLength = 1/3
-note2 = note.Note('F#4')
-note2.quarterLength = 3
+def print_note_info(note):
+    print(f"Duration: {note.duration.quarterLength}")
+    print(f"Offset: {note.offset}")
+    print()
 
-note3 = note.Note('F#4')
-note3.quarterLength = 1/3
+def print_musicxml_info(file_path):
+    # Load the MusicXML file
+    score = converter.parse(file_path)
 
-note4 = note.Note('F#4')
-note4.quarterLength = 1/3
+    # Iterate through all elements in the score
+    for element in score.recurse():
+        if 'Note' in element.classes:
+            print_note_info(element)
 
-note5 = note.Note('F#4')
-note5.quarterLength= 2/3
-bar1 =stream.Part()
-bar1.insert(0, meter.TimeSignature('8/4'))
-
-stream1 = stream.Stream()
-stream1.append(note1)
-stream1.append(note2)
-stream1.append(note3)
-stream1.append(note4)
-stream1.append(note5)
-
-stream1.show()
+# Replace 'your_file.musicxml' with the path to your MusicXML file
+file_path = 'C:\\Users\\USER\\VsCode\\rhythm_generator\\legit.musicxml'
+print_musicxml_info(file_path)
--- test2.py
+++ test2.py
@@ -1,26 +1,26 @@
 from tkinter import *
-from tkinter import ttk
+from tkinter import ttk, messagebox
 from PIL import Image, ImageTk
 from music21 import stream, meter, note
 import os
 import random
-import time
 import threading
 from fractions import Fraction
+import tkinter
 
 rhythm_patterns = [
-    {"name": "sixteenth", "image": "sixteenth_note.png", "value": 0.25},
-    {"name": "Eighth", "image": "eighth_note.jpg", "value": 0.5},
-    {"name": "dotted-Eighth", "image": "dotted_eighth_note.png", "value": 0.75},
-    {"name": "Quarter", "image": "quarter_note.png", "value": 1.0},
-    {"name": "dotted-quarter", "image": "dotted-quarter.png", "value": 1.5},
-    {"name": "Half", "image": "half_note.jpeg", "value": 2},
-    {"name": "dotted-half", "image": "dotted-half.png", "value": 3.0},
-    {"name": "Whole", "image": "whole_note.jpg", "value": 4.0},
-    {"name": "Triplet-eighth", "image": "triplet-eighth.png", "value": Fraction(1/3)},
-    {"name": "Triplet-quarter", "image": "triplet-quarter.png", "value": Fraction(2/3)}
+    {"name": "sixteenth", "image": "sixteenth_note.png", "value": Fraction(1, 4)},
+    {"name": "Eighth", "image": "eighth_note.jpg", "value": Fraction(1, 2)},
+    {"name": "dotted-Eighth", "image": "dotted_eighth_note.png", "value": Fraction(3, 4)},
+    {"name": "Quarter", "image": "quarter_note.png", "value": Fraction(1, 1)},
+    {"name": "dotted-quarter", "image": "dotted-quarter.png", "value": Fraction(3, 2)},
+    {"name": "Half", "image": "half_note.jpeg", "value": Fraction(2, 1)},
+    {"name": "dotted-half", "image": "dotted-half.png", "value": Fraction(3, 1)},
+    {"name": "Whole", "image": "whole_note.jpg", "value": Fraction(4, 1)},
+    {"name": "Triplet-eighth", "image": "triplet-eighth.png", "value": Fraction(1, 3)},
+    {"name": "Triplet-quarter", "image": "triplet-quarter.png", "value": Fraction(2, 3)}
 ]
-BARS_IN_PAGE = 48
+BARS_IN_PAGE = 32
 
 
 class RhythmGeneratorApp:
@@ -31,6 +31,8 @@ class RhythmGeneratorApp:
         # Create a variable to store the selected rhythm patterns
         self.selected_patterns = {}
 
+        self.selected_triplets = {}
+        self.selected_regular = {}
         # Create GUI elements
         self.create_gui_elements()
 
@@ -47,8 +49,10 @@ class RhythmGeneratorApp:
         generate_button = ttk.Button(frm, text="Generate Rhythm", command=self.on_generate_button)
         generate_button.grid(column=1, row=3, columnspan=3)
 
-        # Create buttons for each rhythm pattern
+        close_button = Button(frm, text="Close", command=self.on_close_button)
+        close_button.grid(column=4, row=4, columnspan=3)
 
+        # Create buttons for each rhythm pattern
         images_directory = os.path.join(os.path.dirname(__file__), "images")
 
         for i, pattern in enumerate(rhythm_patterns):
@@ -59,15 +63,15 @@ class RhythmGeneratorApp:
             photo = ImageTk.PhotoImage(image)
 
             column = i % 6
-            row = i //6 +1
+            row = i // 6 + 1
 
-            button = ttk.Checkbutton(frm, text=pattern["name"], image=photo, compound="top", command=lambda p=pattern: self.toggle_pattern(p, button))
-            button.image = photo  
+            button = ttk.Checkbutton(frm, text=pattern["name"], image=photo, compound="top",
+                                     command=lambda p=pattern: self.toggle_pattern(p, button))
+            button.image = photo
             button.grid(column=column, row=row, padx=5, pady=5)
 
 
     def toggle_pattern(self, pattern, button):
-        # Check if the button is in the "pressed" state
         is_pressed = button.instate(['pressed'])
 
         # Toggle the state of the button
@@ -82,36 +86,115 @@ class RhythmGeneratorApp:
             rest_name = f"rest-{pattern['name']}"
             del self.selected_patterns[rest_name]
         else:
-            self.selected_patterns[pattern["name"]] = float(pattern["value"])
+            self.selected_patterns[pattern["name"]] = pattern["value"]
             rest_name = f"rest-{pattern['name']}"
-            self.selected_patterns[rest_name] = pattern["value"]
+            self.selected_patterns[rest_name] = Fraction(1, 3) if pattern["name"] == "Triplet-eighth" else pattern["value"]
 
-        print("Selected Rhythm Patterns:", self.selected_patterns)
-            
+        # Update the selected triplets dictionary
+        self.update_selected_triplets()
+        self.update_selected_regular()
+
+    def update_selected_triplets(self):
+        self.selected_triplets = {name: value for name, value in self.selected_patterns.items() if "Triplet" in name}
+    
+    def update_selected_regular(self):
+        self.selected_regular = {name: value for name, value in self.selected_patterns.items() if not "Triplet" in name}  
+
     def generate_oneBar(self):
-        bar_length = Fraction(4)
-        bar = Fraction(0)
-        tolerance = Fraction(1, 10)
+
+        bar_length = Fraction(4, 1)
+        bar = Fraction(0, 1)
         selected_rhythm = []
+        eighth_triplet = Fraction(1, 3)
+        quarter_triplet = Fraction(2, 3)
+        quarter = Fraction(1, 1)
+        fourth_beat = Fraction(3, 1)
+        tries = 0
+        max_tries = 20
 
-        while bar < bar_length and self.selected_patterns:
+        while bar < bar_length and self.selected_patterns and (self.selected_regular or self.selected_triplets):
             selected_note_name = random.choice(list(self.selected_patterns))
-            selected_note = Fraction(self.selected_patterns[selected_note_name])
+            selected_note = self.selected_patterns[selected_note_name]
+            if tries >= max_tries:
+                error_message = "Exceeded maximum number of tries. Unable to generate valid rhythm."
+                tkinter.messagebox.showerror("Error", error_message)
+                raise ValueError(error_message)
+            
+            if selected_note + bar > bar_length:
+                tries += 1
+                continue
 
-            if selected_note == Fraction(1, 3) or selected_note == Fraction(2, 3):
-                if bar + selected_note <= bar_length:
-                    bar += selected_note
+            elif bar % quarter == 0 and bar + selected_note <= bar_length and selected_note:
+                if selected_note == eighth_triplet:
+                    triplet_notes = self.generate_eighth_triplets()
+                    for note_name, duration in triplet_notes:
+                        bar += duration
+                        selected_rhythm.append((note_name, duration))
+
+                elif selected_note == quarter_triplet and bar == fourth_beat:
+                    triplet_notes = self.generate_eighth_triplets()
+                    for note_name, duration in triplet_notes:
+                        bar += duration
+                        selected_rhythm.append((note_name, duration))   
+
+                elif selected_note == quarter_triplet:
+                    triplet_notes = self.generate_quarter_triplets()
+                    for note_name, duration in triplet_notes:
+                        bar += duration
+                        selected_rhythm.append((note_name, duration))
+
+                elif self.selected_regular:
+                    bar += selected_note                    
                     selected_rhythm.append((selected_note_name, selected_note))
-            else:
-                if bar + selected_note <= bar_length:
-                    bar += selected_note
-                    selected_rhythm.append((selected_note_name, selected_note))
 
-        # Round the durations for MuseScore
-        selected_rhythm_rounded = [(note, round(float(duration), 4)) for note, duration in selected_rhythm]
+            elif bar + selected_note <= bar_length and self.selected_regular:
+                selected_note_name = random.choice(list(self.selected_regular))
+                selected_note = self.selected_regular[selected_note_name]
+                while selected_note + bar > bar_length:
+                    selected_note_name = random.choice(list(self.selected_regular))
+                    selected_note = self.selected_regular[selected_note_name]
+                bar += selected_note
+                selected_rhythm.append((selected_note_name, selected_note))
 
-        return selected_rhythm_rounded
-                
+        # print(f"Bar: {selected_rhythm}")
+        return selected_rhythm
+    
+    def generate_eighth_triplets(self):
+        triplets_notes = []
+        quarter = Fraction(1, 1)
+        total = Fraction(0, 1)
+
+        while total < quarter:
+            selected_note_name = random.choice(list(self.selected_triplets))
+            selected_note = self.selected_triplets[selected_note_name]
+
+            if total + selected_note > quarter:
+                continue
+
+            total += selected_note
+            triplets_notes.append((selected_note_name, selected_note))
+
+        return triplets_notes
+        
+    def generate_quarter_triplets(self):
+        triplet_notes = []
+        half = Fraction(2, 1)
+        total = Fraction(0, 1)
+
+        while total < half:
+            selected_note_name = random.choice(list(self.selected_triplets))
+            selected_note = self.selected_triplets[selected_note_name]
+            if total + selected_note > half:
+                continue
+
+            if total == Fraction(5, 3):
+                while selected_note != Fraction(1, 3):
+                    selected_note_name = random.choice(list(self.selected_triplets))
+                    selected_note = self.selected_triplets[selected_note_name]
+            total += selected_note
+            triplet_notes.append((selected_note_name, selected_note))
+
+        return triplet_notes
 
     def generate_rhythms(self):
         for bar in range(BARS_IN_PAGE):
@@ -121,18 +204,24 @@ class RhythmGeneratorApp:
             except StopIteration:
                 print("StopIteration in generate_rhythms")
 
-        print("Generation complete")
 
     def create_music_stream(self, rhythms):
         music_stream = stream.Stream()
-        
+        music_stream.append(meter.TimeSignature('4/4'))
+
         for rhythm in rhythms:
-            for note_name, duration in rhythm:
+            measure = stream.Measure()
+
+            for index, (note_name, duration) in enumerate(rhythm, start=1):
                 choices = [note.Note('C4'), note.Rest()]
                 probability = [0.8, 0.2]
-                n = random.choices(choices, weights= (probability))[0]
+                n = random.choices(choices, weights=probability)[0]
+                # print(f"Iteration {index}: {duration}")
                 n.quarterLength = duration
-                music_stream.append(n)
+                measure.append(n)
+
+            music_stream.append(measure)
+
         return music_stream
 
 
@@ -145,8 +234,16 @@ class RhythmGeneratorApp:
             rhythms = list(self.generate_rhythms())
             music_stream = self.create_music_stream(rhythms)
             music_stream.show()
+            print("Generation complete")
+
         except StopIteration:
             print("Generation complete")
+
+        except ValueError:
+            pass
+
+    def on_close_button(self):
+        self.root.destroy()
 
 
 if __name__ == "__main__":
